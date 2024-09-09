c1fd04c438848358f2148d7cfa2d3b228e33d143
Anas Obaid <anasobaidpk@gmail.com>
2024-09-09 19:07:15 +0500

    Fix all endpoint issues

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Inject, Post } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignupDto } from './dtos/signup.dto';
 import { SigninDto } from './dtos/signin.dto';
@@ -6,13 +6,13 @@ import { SigninDto } from './dtos/signin.dto';
 @Controller('auth')
 export class AuthController {
   constructor(@Inject() private authService: AuthService) {}
-  @Post()
-  signup(signUpDto: SignupDto) {
+  @Post('signup')
+  signup(@Body() signUpDto: SignupDto) {
     return this.authService.signup(signUpDto);
   }
 
-  @Get()
-  signin(signinDto: SigninDto) {
+  @Get('signin')
+  signin(@Body() signinDto: SigninDto) {
     return this.authService.signin(signinDto);
   }
 }
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Get, Inject, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Inject,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dtos/create-task.dto';
 import { UpdateTaskDto } from './dtos/update-task.dto';
@@ -17,15 +26,17 @@ export class TasksController {
     return this.tasksService.findAll();
   }
 
-  @Get()
+  @Get(':id')
   findOne(@Param('id') id: string) {
     return this.tasksService.findOne(+id);
   }
 
+  @Patch(':id')
   updateTask(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
     return this.tasksService.update(+id, updateTaskDto);
   }
 
+  @Delete(':id')
   deleteTask(@Param('id') id: string) {
     return this.tasksService.delete(+id);
   }
